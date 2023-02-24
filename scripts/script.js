1f7839ca767d1bc2b7cd42a5173996f57440a3c7
Gaganjit Heer <heergaganjit@gmail.com>
2023-02-24 01:17:42 -0800

    Created home page with placeholders and getName function

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,20 @@ function loadSkeleton(){
     console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
 loadSkeleton();  //invoke the function
+
+function getNameFromAuth() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) { 
+            console.log(user.uid); 
+            console.log(user.displayName);  
+            user_Name = user.displayName;
+
+            $("#name-goes-here").text(user_Name); 
+
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+getNameFromAuth(); //run the function
