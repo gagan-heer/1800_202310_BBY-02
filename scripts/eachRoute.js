2b1cfa4da4b4bfd06432410ce18d00d9da960caf
Arcie Lao <alao10@my.bcit.ca>
2023-03-05 00:56:50 -0800

    added event listener and fixed template.html css

---
+++ scripts/eachRoute.js
@@ -0,0 +1,22 @@
+function displayRouteInfo() {
+    let params = new URL( window.location.href ); //get URL of search bar
+    let ID = params.searchParams.get( "docID" ); //get value for key "id"
+    console.log( ID );
+
+    // doublecheck: is your collection called "Reviews" or "reviews"?
+    db.collection( "routes" )
+        .doc( ID )
+        .get()
+        .then( doc => {
+            thisRoute = doc.data();
+            routeCode = thisRoute.code;
+            routeTitle = doc.data().name;
+            
+            // only populate title, and image
+            document.getElementById( "routeName" ).innerHTML = routeTitle;
+            document.getElementById("route-image").src("https://dummyimage.com/400x315/a6a6a6/fff");
+            // let imgEvent = document.querySelector( "route-img" );
+            // imgEvent.src = "https://dummyimage.com/400x315/a6a6a6/fff";
+        } );
+}
+displayRouteInfo();
--- scripts/main.js
+++ scripts/main.js
@@ -153,12 +153,14 @@ function displayCardsDynamically(collection) {
                 var title = doc.data().name;       // get value of the "name" key
                 var details = doc.data().details;  // get value of the "details" key
 				var routeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                var docID = doc.id;
                 let newcard = cardTemplate.content.cloneNode(true);
 
                 //update title and text and image
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `https://dummyimage.com/400x315/a6a6a6/fff`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "eachRoute.html?docID=" + docID;
 
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
