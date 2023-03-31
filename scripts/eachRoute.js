96faaf9974190aecf10480ddacc346d6751ca71c
Arcie Lao <alao10@my.bcit.ca>
2023-03-31 16:49:47 -0700

    fixed favourite bug

--- scripts/eachRoute.js
+++ scripts/eachRoute.js
@@ -77,61 +77,10 @@ function displayRouteInfo() {
 }
 displayRouteInfo();
 
-let count = 0;
-//the lat/long is from the favourite collections so that only favourite routes are parsed through
-function updatedHours(lat, long, title){
-  ajaxGET(urlEvent(lat, long), function (data) {
-    parsedData = JSON.parse(data);
-    let currentTime = new Date();
-    var sentence = "";
-
-    parsedData.events.sort(function(a,b){
-      // Turn your strings into dates, and then subtract them
-      // to get a value that is either negative, positive, or zero.
-      return new Date(b.updated) - new Date(a.updated);
-    });
-
-    if(parsedData.events.length != 0){
-      let difference_s = (new Date(parsedData.events[0].updated).getTime() - currentTime.getTime()) / -1000;
-      let difference_m = difference_s / 60;
-      let difference_h = difference_m / 60;
-      if(difference_h <= 250){
-        sentence += "<div id=\"notification-title\">" + title + "</div>";
-      }
-    }
-
-
-
-    for(let i = 0; i < parsedData.events.length; i++){
-      let difference_s = (new Date(parsedData.events[i].updated).getTime() - currentTime.getTime()) / -1000;
-      let difference_m = difference_s / 60;
-      let difference_h = difference_m / 60;
-      let temp = parsedData.events[i].description;
-      let hour_difference = parseInt(difference_h);
-    
-      //if you change the hour difference make sure to change the one above too
-      if(difference_h <= 250 ){ //this is looking for all favourite route's events less than 500h ago.
-        //because of cutting words off some text may look the same even though they are entirely different events
-        //discuss what to do, wether to keep short text or give long text. or find more patterns with their desc.
-        if(temp.indexOf("Starting") > -1){
-          sentence = sentence.concat("<li><a class=\"dropdown-item\" href=\"#\" id=\"content-", count, "\">", temp.substring(0, temp.indexOf("Starting")), hour_difference," Hours ago</a></li>");
-        } else {
-          sentence = sentence.concat("<li><a class=\"dropdown-item\" href=\"#\" id=\"content-", count, "\">", temp.substring(0, temp.indexOf("Until")),"</a></li>");
-        }
-        findRouteLink(lat,long, count);
-        count++;
-      }
-    }
-    document.getElementById("dropdown-notifications").innerHTML += sentence;
-
-    document.getElementById("notification-count").innerHTML = count;
-  });
-}
-
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
       const userId = user.uid;
-      displayFavsDynamically("favourites", userId);
+      // displayFavsDynamically("favourites", userId);
     } else {
       console.log("User is not logged in.");
     } 
