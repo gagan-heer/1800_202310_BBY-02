35a97111a681749de5178d3f2eb9fed19f1d9e64
Gaganjit Heer <heergaganjit@gmail.com>
2023-04-06 16:15:27 -0700

    Fixed user feedback function

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,18 +1,34 @@
-function saveUserFeedback(routeId, feedback) {
-    const userId = firebase.auth().currentUser.uid; // Get the ID of the currently logged in user
-  
-    // Create a reference to the user's feedback collection for the specific route
-    const feedbackRef = firebase.firestore().collection("users").doc(userId).collection("feedback").doc(routeId);
-  
-    feedbackRef.set({
-      feedback: feedback,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    })
-    .then(() => {
-      console.log("Feedback saved successfully.");
-    })
-    .catch((error) => {
-      console.error("Error saving feedback: ", error);
+// Saves user feedback to Firestore 
+  function saveUserFeedback() {
+    console.log("inside write review")
+    let input1 = document.getElementById("input1").value;
+    let input2 = document.getElementById("input2").value;
+    let input3 = document.getElementById("input3").value;
+    let description = document.getElementById("description").value;
+    console.log(input1, input2, input3, description);
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid)
+            var userID = user.uid;
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    var userEmail = userDoc.data().email;
+                    db.collection("feedback").add({
+                        userID: userID,  
+                        input1: input1,
+                        input2: input2,
+                        input3: input3,
+                        description: description,
+                        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                    }).then(() => {
+                        window.location.href = "thanks.html"; 
+                    })
+                })
+        } else {
+            console.log("No user is signed in");
+            window.location.href = 'review.html';
+        }
     });
-  }
- 
+}
